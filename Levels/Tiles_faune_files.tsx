91202bceefd884d256093fe615f477085f5a439e
jesuggc <jesugo11@ucm.es>
2023-03-02 14:47:14 +0100

    Prueba 2 con mapa hito 2

---
+++ Levels/Tiles_faune_files.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tiles_faune_files" tilewidth="16" tileheight="16" tilecount="464" columns="29">
+ <image source="GitHub/zugarramundisescape/Recursos/legend of faune files/legend of faune files/Environment/PNG/tiles.png" width="464" height="256"/>
+</tileset>
