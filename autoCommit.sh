4678ce7f01782f832eeeb46271426a832a11e3b5
jesuggc <jesugo11@ucm.es>
2023-03-21 23:26:38 +0100

    Autocommit by jesuggc: Commit con el script

---
+++ autoCommit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get the number argument passed to the script
+commit_message="$1"
+author_name=$(git config user.name)
+status=$(git status)
+
+# Check if the number argument is empty
+if [ -z "$commit_message" ]
+then
+    final_message="Autocommit without comment by $author_name"  
+else
+    final_message="Autocommit by $author_name: $commit_message"
+fi
+
+# Run the Git commands
+git status
+git add -A
+git commit -m "$final_message"
+git push origin main
+
+read -p "Press Enter to exit."
