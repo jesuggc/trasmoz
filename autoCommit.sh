724f8ff5798fb0446aff6e3ee792561e643414e9
jesuggc <jesugo11@ucm.es>
2023-03-23 22:38:17 +0100

    Autocommit without comment by jesuggc

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Get the number argument passed to the script
 commit_message="$1"
 author_name=$(git config user.name)
 status=$(git status)
 
-# Check if the number argument is empty
 if [ -z "$commit_message" ]
 then
     final_message="Autocommit without comment by $author_name"  
@@ -13,7 +11,6 @@ else
     final_message="Autocommit by $author_name: $commit_message"
 fi
 
-#check if status has the sentence "nothing to commit"
 if echo "$status" | grep -q "nothing to commit"
 then
     echo -e "\e[41mNO CHANGES TO COMMIT\e[0m" #fondo rojo
