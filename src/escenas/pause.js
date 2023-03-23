3ebb14ff6fca8111952a157339e467f3f26c3e34
Kutsoragi <fferna07@ucm.es>
2023-03-23 14:28:15 +0100

    lifebar

--- src/escenas/pause.js
+++ src/escenas/pause.js
@@ -10,7 +10,16 @@ export default class Title extends Phaser.Scene {
 	}
 	 
 	preload(){
-		this.load.image('resume', 'assets/GUI/play_button.png');
+        this.load.image('play', 'assets/GUI/playButton1.png');
+		this.load.image('play2', 'assets/GUI/playButton2.png');
+		this.load.image('credits', 'assets/GUI/creditsButton1.png');
+		this.load.image('credits2', 'assets/GUI/creditsButton2.png');
+		this.load.image('options', 'assets/GUI/optionsButton1.png');
+		this.load.image('options2', 'assets/GUI/optionsButton2.png');
+		this.load.image('sound', 'assets/GUI/soundButton1.png');
+		this.load.image('sound2', 'assets/GUI/soundButton2.png');
+		this.load.image('fullscreen', 'assets/GUI/fullscreenButton1.png');
+		this.load.image('fullscreen2', 'assets/GUI/fullscreenButton2.png');
         this.load.image('pause_background', 'assets/GUI/pause_background.jpg')
 	}
 
@@ -27,14 +36,50 @@ export default class Title extends Phaser.Scene {
             repeat: 0
           });
           tween.on('complete', () => {
-              var sprite = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 'resume')
-              sprite.setInteractive();
-              sprite.setScale(0.3)
-              sprite.on('pointerup', () => {
-                  console.log("pulso resume")
-                  this.scene.resume('animation'); 
-                  this.scene.stop()
-              });
+            this.playButton = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/4, 'play').setInteractive();
+            this.fullscreenButton = this.add.image(this.sys.game.canvas.width/9.9, this.sys.game.canvas.height/1.2, 'fullscreen').setInteractive();
+            this.optionsButton = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 'options').setInteractive();
+            this.optionsButton2 = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 'options2').setInteractive();
+            this.creditsButton = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/(4/3), 'credits').setInteractive();
+            this.soundButton = this.add.image(this.sys.game.canvas.width/1.1, this.sys.game.canvas.height/1.2, 'sound').setInteractive();
+            // Escuchamos los eventos del raton cuando interactual con nuestro sprite de "Start"
+            this.fullscreenButton.on('pointerup', function () {
+                console.log("Entro fullscreen")
+                if (this.scale.isFullscreen)
+                {
+                    this.scale.stopFullscreen();
+                }
+                else
+                {
+                    this.game.scale['autoCenter'] = Phaser.Scale.CENTER_BOTH;
+                    this.game.scale.displaySize['maxWidth'] = 8000;
+                    this.game.scale.displaySize['maxHeight'] = 8000;
+                    this.scale.startFullscreen();
+                }
+    
+            }, this);
+            
+    
+    
+            this.scale.on('leavefullscreen', function () {
+                this.game.scale['autoCenter'] = Phaser.Scale.CENTER_HORIZONTALLY;
+                    this.game.scale.displaySize['maxWidth'] = 900;
+                    this.game.scale.displaySize['maxHeight'] = 750;
+            }, this);
+    
+    
+            this.playButton.on('pointerup', () => {
+                this.playButton.destroy()
+                this.playButton = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/4, 'play2')
+                this.time.addEvent({delay: 100, callback: function(){
+                this.playButton.destroy();
+                    this.playButton = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/4, 'play').setInteractive();
+                    this.time.addEvent({delay: 400, callback: function(){
+                    this.scene.resume('animation');
+                    this.scene.stop()
+                    }, callbackScope: this});
+                }, callbackScope: this});
+            });
           })
 		
 
