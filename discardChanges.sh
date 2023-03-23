25fe3e4813a53bf777b444fb434bf4bf19093866
jesuggc <jesugo11@ucm.es>
2023-03-23 22:47:09 +0100

    Autocommit without comment by jesuggc

--- discardChanges.sh
+++ discardChanges.sh
@@ -4,4 +4,4 @@ git fetch
 git reset --hard origin/main
 
 branch= $(git branch --show-current)
-"\n\e[42;37mLOCAL CHANGES DISCARDED. REMOTE REPOSITORY $branch DOWNLOADED\e[0m"
+echo -e "\n\e[42;37mLOCAL CHANGES DISCARDED. REMOTE REPOSITORY $branch DOWNLOADED\e[0m"
