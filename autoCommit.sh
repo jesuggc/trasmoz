445e71e5f2d9ad5f298dde703c9498e2773db62b
jesuggc <jesugo11@ucm.es>
2023-03-23 21:19:07 +0100

    Autocommit without comment by jesuggc

--- autoCommit.sh
+++ autoCommit.sh
@@ -13,8 +13,16 @@ else
     final_message="Autocommit by $author_name: $commit_message"
 fi
 
-# Run the Git commands
-git status
-git add -A
-git commit -m "$final_message"
-git push origin main
+
+if [ -z "$status" ]
+then
+    echo -e "\e[41mNO CHANGES TO COMMIT\e[0m" #fondo rojo
+else
+    git status
+    git add -A
+    git commit -m "$final_message"
+    git push origin main
+    echo -e "\e[42;37mALL CHANGES COMMITED\e[0m" #fondo verde
+fi
+#What does -z do
+#I want to add a check to see if there are any changes to commit before running the git commands. I tried to use the status variable but it doesn't work. I tried to use the following code but it doesn't work either:
--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-echo -e "\e[32mhola\e[0m"
+echo -e "\e[92mhola\e[0m"
+#Create an echo with red background color
+echo -e "\e[42;37mhola\e[0m" #fondo verde
+echo -e "\e[47mhola\e[0m" #fondo blanco
+echo -e "\e[41mhola\e[0m" #fondo rojo
