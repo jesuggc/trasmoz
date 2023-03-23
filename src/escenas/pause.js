15d6bdf26458ffd4b57e75636c51b02c0fa56d09
jesuggc <jesugo11@ucm.es>
2023-03-23 12:29:43 +0100

    Autocommit without comment by jesuggc

--- src/escenas/pause.js
+++ src/escenas/pause.js
@@ -1,24 +1,18 @@
 /**
- * Escena de Titulo.
  * @extends Phaser.Scene
  */
 export default class Title extends Phaser.Scene {
 	/**
-	 * Escena principal.
 	 * @extends Phaser.Scene
 	 */
 	constructor() {
 		super({ key: 'pause' });
 	}
-
-	//Cargamos todos los assets que vamos a necesitar
 	 
 	preload(){
 		this.load.image('resume', 'assets/GUI/play_button.png');
         this.load.image('pause_background', 'assets/GUI/pause_background.jpg')
 	}
-	//TODO #3
-	//Creacion de los elementos de la escena principal de juego
 
 	create() {
 		console.log("Entrando en pausa")
@@ -33,13 +27,12 @@ export default class Title extends Phaser.Scene {
             repeat: 0
           });
           tween.on('complete', () => {
-              //Pintamos un boton de Empezar
               var sprite = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 'resume')
-              sprite.setInteractive(); // Hacemos el sprite interactivo para que lance eventos
+              sprite.setInteractive();
               sprite.setScale(0.3)
               sprite.on('pointerup', () => {
                   console.log("pulso resume")
-                  this.scene.resume('animation'); //Cambiamos a la escena de juego
+                  this.scene.resume('animation'); 
                   this.scene.stop()
               });
           })
