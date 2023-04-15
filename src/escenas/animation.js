6adc0bd163734fe7e8eff328bf54da46fd484178
jesuggc <jesugo11@ucm.es>
2023-04-15 15:08:08 +0200

    some minor changes

--- src/escenas/animation.js
+++ src/escenas/animation.js
@@ -61,7 +61,7 @@ export default class Animation extends Phaser.Scene {
 		this.witch = new Witch(this, 1000, 7450);		
 		this.physics.add.collider(this.witch, this.colisiones);
 		this.muchosLobos = this.add.group();
-		for (var i = 0; i < 10; i++) {
+		for (var i = 0; i < 0; i++) {
 			let wolf = new Wolf(this, Math.random() * 10, Math.random() * 10);
 			this.muchosLobos.add(wolf);
 		}
