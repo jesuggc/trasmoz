9c52248c5af277652d67aa51c4da8cb34abb6703
jesuggc <jesugo11@ucm.es>
2023-04-15 02:24:45 +0200

    several tileset working on same project, names must be fixed

--- src/escenas/animation.js
+++ src/escenas/animation.js
@@ -17,8 +17,15 @@ export default class Animation extends Phaser.Scene {
 		this.load.spritesheet('expBall', 'assets/Bruja/expBall.png', { frameWidth: 19, frameHeight: 18 })
 		this.load.spritesheet('wolf', 'assets/enemies/wolfWalk.png', { frameWidth: 64.8, frameHeight: 33 })
 		this.load.spritesheet('fireFlower', 'assets/GUI/fireFlower.png', { frameWidth: 479, frameHeight: 576 })
-		this.load.tilemapTiledJSON('tilemap', 'levels/Mapa_inicial.json');
-		this.load.image('patronesTilemap', 'levels/tiles.png');
+		this.load.tilemapTiledJSON('tilemap', 'levels/MapaPrueba.json'); //AQUI
+		this.load.image('patronesTilemap', 'levels/tilesPrueba.png'); //AQUI
+		this.load.image('patronArboles', 'levels/Trees.png'); //AQUI
+		this.load.image('patronCasa', 'levels/witchHouse (6).png'); //AQUI
+		this.load.image('patronesAcantilado', 'levels/CLiff.png'); //AQUI
+		this.load.image('patronesPiedras', 'levels/Rocks.png'); //AQUI
+		this.load.image('patronesTumbas', 'levels/TileserGraveyard.png'); //AQUI
+		this.load.image('patronesTroncos', 'levels/undefined_-_Imgur.png'); //AQUI
+
 		this.load.image('pause_button', 'assets/GUI/pause_button.png')
 		this.load.image('noname', 'assets/noname/noName1.png');
 		this.load.image('noname2', 'assets/noname/noName2.png');
@@ -34,15 +41,24 @@ export default class Animation extends Phaser.Scene {
 			width: 60,
 			height: 40
 		});
-		const tileset1 = this.map.addTilesetImage('Tiles_faune_files', 'patronesTilemap');
-		this.suelo = this.map.createLayer('Suelo', tileset1);
-		this.colisiones = this.map.createLayer('Colliders', tileset1);
+		const tileset1 = this.map.addTilesetImage('Ground Tileset', 'patronesTilemap'); //AQUI
+		const tileset2 = this.map.addTilesetImage('Trees', 'patronArboles'); //AQUI
+		const tileset3 = this.map.addTilesetImage('witchHouse (6)', 'patronCasa'); //AQUI
+		const tileset4 = this.map.addTilesetImage('Cliff', 'patronesAcantilado'); //AQUI
+		const tileset5 = this.map.addTilesetImage('Rocks', 'patronesPiedras'); //AQUI
+		const tileset6 = this.map.addTilesetImage('TilesetGraveyard', 'patronesTumbas'); //AQUI
+		const tileset7 = this.map.addTilesetImage('undefined_-_Imgur', 'patronesTroncos'); //AQUI
+
+		this.suelo = this.map.createLayer('Suelo',  [ tileset1,tileset2,tileset3,tileset4,tileset5,tileset6,tileset7 ]);
+		this.colisiones = this.map.createLayer('Colliders', [ tileset1,tileset2,tileset3,tileset4,tileset5,tileset6,tileset7 ]);
+		this.arboles = this.map.createLayer('Arboles', [ tileset1,tileset2,tileset3,tileset4,tileset5,tileset6,tileset7 ]);
+
 		this.colisiones.setCollisionByExclusion(-1);
 		this.noname1;
 		this.noname2;
 		this.spawnDistance = 280;
 		new FireFlower(this,0,0);
-		this.witch = new Witch(this, 300, 300);		
+		this.witch = new Witch(this, 1000, 7450);		
 		this.physics.add.collider(this.witch, this.colisiones);
 		this.muchosLobos = this.add.group();
 		for (var i = 0; i < 10; i++) {
@@ -51,7 +67,6 @@ export default class Animation extends Phaser.Scene {
 		}
 		this.physics.add.collider(this.witch, this.muchosLobos, this.witch.perderVida, null, this.witch)
 		this.physics.add.collider(this.muchosLobos, this.colisiones);
-		this.physics.add.collider(this.witch, this.muchosLobos, this.perderVida, null, this);
 		this.physics.add.collider(this.muchosLobos,this.muchosLobos);
 		
 		if(Math.random() < 0.95) {
@@ -123,7 +138,7 @@ export default class Animation extends Phaser.Scene {
 		this.cameras.main.startFollow(this.witch);
 
 		this.events.on('resume', ( sys, skill) =>{
-			if (skill)this.witch[skill.skillSelected]()
+			if (skill) this.witch[skill.skillSelected]()
 		})
 		
 	}
