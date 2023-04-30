98b48dd7fac79bcb9b894e655ed3642e856c8927
Fran <fcubeddev@gmail.com>
2023-05-01 00:35:33 +0200

    esto ya está

--- src/escenas/gameover.js
+++ src/escenas/gameover.js
@@ -6,8 +6,7 @@ export default class Gameover extends Phaser.Scene {
 	create() {
 		this.defeat = this.add.image(this.sys.game.config.width/2, this.sys.game.config.height/2, 'gameoverImage').setScrollFactor(0).setInteractive();
 		this.defeat.on('pointerup', () => {
-			this.scene.launch('animation');
-			this.scene.stop()
+			location.reload();
 		})
         
 	}
--- src/escenas/win.js
+++ src/escenas/win.js
@@ -7,8 +7,7 @@ export default class Win extends Phaser.Scene {
 	create() {
 		this.defeat = this.add.image(this.sys.game.config.width/2, this.sys.game.config.height/2, 'winImage').setScrollFactor(0).setInteractive();
 		this.defeat.on('pointerup', () => {
-			this.scene.launch('title');
-			this.scene.stop()
+			location.reload();
 		})
         
 	}
